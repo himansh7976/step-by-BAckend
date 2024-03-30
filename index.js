9473bead59cb24cf822f53f6ed9085395091fc23
himanshu <himansh7976@gmail.com>
2024-03-30 16:16:36 +0530

    add

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 
 // let a=20
 // let b=30
-// const arr=[5,8,9,4,5,50];
+//  const arr=[ 5,8,9,4,5,50];
+//  let a=arr.find((e)=>{
+//   return e==5
+
+//  })
+
+//  console.log(a);
 //  let res=arr.filter((i)=>{
 
 //     return i>5
@@ -59,6 +65,9 @@
 // make a smiple api'
 
 
+
+
+
 // const http = require("http");
 // const data = require("./app")
 
@@ -71,16 +80,194 @@
 // http.createServer(fun).listen(4000)
 
 // =============
-const express = require('express')
-const product = require('./data')
-const app = express();
+// const express = require('express')
+// const product = require('./data')
+// const app = express();
 
-app.get('/', (req, res) => {
-   res.send("hello")
-})
+// app.get('/', (req, res) => {
+//    res.send("hello")
+// })
 
-app.get('/products:brand', (req, res) => {
-   const a = product.filter((i) => i.brand.toLowerCase() === req.params.brand.toLowerCase())
-   res.send(a)
-})
-app.listen(3001)        
+// app.get('/products:brand', (req, res) => {
+//    const a = product.filter((i) => i.brand.toLowerCase() === req.params.brand.toLowerCase())
+//    res.send(a)
+// })
+// app.listen(3001)        
+
+// Curd file system'
+// make file using fs'
+
+
+// const fs=require('fs')
+// fs.writeFileSync("apple.text","appple ji");
+
+
+// ======================
+
+//  syncronors and asyncronos program
+
+
+
+
+
+
+
+
+
+// console.log("start");
+// console.log("llogive");
+// console.log("kokok");
+// setTimeout(()=>{
+//    console.log("hello")
+
+// },3000)
+
+// console.log("333")
+
+// ======================================
+// asyncronos handle'
+
+
+
+// let a = 20;
+// let b = 0;
+
+
+
+
+// let wait = new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//       b = 30;
+//       resolve(30)
+
+//    }, 3000)
+// })
+
+// wait.then((u)=>{
+//    console.log(u)
+// })
+
+// console.log(a + b);
+
+
+// ===============================
+
+// Express js/
+
+// let express=require("express");
+// const app=express();
+
+// app.get('',(req,res)=>{
+//    res.send("hello ji")
+// })
+
+// app.get('/about',(req,res)=>{
+//    console.log(req.query.name)
+//    res.send("hello "+req.query.name)
+
+// })
+
+
+
+// app.listen(5010);
+// ===================
+//================express js======/
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     console.log(req.query)
+//    res.send("hello")
+
+
+
+// })
+
+
+// app.get('/aboutt', (req, res) => {
+//     res.send('hello about')
+// })
+
+// app.get('/aboutt/ha', (req, res) => {
+//     console.log(req.query.name)
+//     res.send('hello about')
+// })
+
+
+
+
+// app.listen(8001)
+
+
+
+
+// ======================================
+
+// render html and json' 
+
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     console.log(req.query)
+//     res.send(`<h1>hello</h1>
+//     <a href="/aboutt">go to about</a>
+    
+//     `)
+
+
+
+// })
+
+
+// app.get('/aboutt', (req, res) => {
+//     res.send({
+//         name:"anil",
+//         email:"hiamshu@fuiui.com"
+//     })
+// })
+// app.listen(8001)
+
+
+const express=require('express');
+const app=express();
+
+
+
+app.listen(5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
